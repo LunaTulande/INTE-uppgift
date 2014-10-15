096b4e897cdfaf3d647889bf97d9af98b87eabc5
nazgir <erik.lundgren1@gmail.com>
2014-10-15 20:19:33 +0200

    initial netbeans commit

---
+++ src/src/src/Src.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+/**
+ *
+ * @author Erik Lundgren <erik at lundgren.cc>
+ */
+public class Src {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
